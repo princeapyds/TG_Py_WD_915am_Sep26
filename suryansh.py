b6411b0b23a714822899a95c7a5c92c956f35871
Suryansh <suryanshgosai1234@gmail.com>
2026-09-18 09:32:41 +0530

    [001]:test commited

--- suryansh.py
+++ suryansh.py
@@ -4,7 +4,9 @@ print(f'my name is {name} and I am {age} years old')
 
 print('I have strong fundamentals in python..')
 
-print('This is my 5th commit after manipulating file')
+print("this is new line!! ")
+
+# print('This is my 5th commit after manipulating file') # i make it comment
 
 print("currently i am in pune")
 print("It is commited")
